8ced6e655dacfe283f687f42da26031de0158a81
realGuybrush <TripleBaka@yandex.ru>
2021-07-19 23:23:39 +0300

    Added cursor following for SpaceShip.

--- SwissAstroKnight/Assets/Scripts/Knight/KnightControls.cs
+++ SwissAstroKnight/Assets/Scripts/Knight/KnightControls.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KnightControls : MonoBehaviour
+public partial class KnightControls : MonoBehaviour
 {
+    Camera mainCam;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        FollowCursor();
     }
 }
---
+++ SwissAstroKnight/Assets/Scripts/Knight/KnightControlsFollowCursor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class KnightControls : MonoBehaviour
+{
+    public GameObject rotatingPart;
+    private void FollowCursor()
+    {
+        float dX, dY;
+        float gipotenuza;
+        float preBeta;
+        dX = mainCam.ScreenToWorldPoint(Input.mousePosition).x - this.transform.position.x;
+        dY = mainCam.ScreenToWorldPoint(Input.mousePosition).y - this.transform.position.y;
+        gipotenuza = (float)Mathf.Sqrt(dX * dX + dY * dY);
+        preBeta = (float)(Mathf.Rad2Deg * Mathf.Asin(dY / gipotenuza));
+        if (dX < 0)
+            preBeta = Mathf.Sign(preBeta) * 180 - preBeta;
+        preBeta -= 90;
+        rotatingPart.transform.eulerAngles = new Vector3(0.0f, 0.0f, preBeta);
+    }
+}
